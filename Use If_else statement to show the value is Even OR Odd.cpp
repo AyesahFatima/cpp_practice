701e8a2f2fac5b5de823c4eb17554c5ac9f16e36
AyesahFatima <111555221+AyesahFatima@users.noreply.github.com>
2022-08-19 04:55:56 -0700

    Create Use If_else statement to show the value is Even OR Odd.cpp

---
+++ Use If_else statement to show the value is Even OR Odd.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int valueone;
+	int valuetwo;
+	cout<<"please enter valueone";
+	cin>>valueone;
+	cout<<"please enter valuetwo";
+	cin>>valuetwo;
+	if(valueone%2)
+	{
+		cout<<"the value is even";
+	}
+	else
+	{
+		cout<<"the value is odd";
+	}
+	
+}
+	
