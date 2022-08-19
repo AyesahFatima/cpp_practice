ed67c24d042faf47b3ba36c77611e7703d3aac17
AyesahFatima <111555221+AyesahFatima@users.noreply.github.com>
2022-08-19 03:47:55 -0700

    Create Add Two values.cpp

---
+++ Add Two values.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int valueOne;
+	int valueTwo;
+	cout<<" Please Enter the valuOne";
+	cin>>valueOne;
+	cout<<"Please Enter the valueTwo";
+	cin>>valueTwo;
+	int Result=valueOne+valueTwo;
+	cout<<Result;
+}
